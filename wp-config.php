ab4e713b3ffdde46423394ec87ca517a9bd1999e
cristhians13 <cristhiansampaio@gmail.com>
2022-11-08 17:07:24 -0300

    Edit Jenkinsfile - Deploy

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mysql' );
+define( 'DB_NAME', '${env.DB_NAME}' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', '${env.DB_USER}' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'JAVA>PHPin2012IN!2018' );
+define( 'DB_PASSWORD', '${env.DB_PASSWORD}' );
 
 /** Database hostname */
-define( 'DB_HOST', '10.10.10.6:3306' );
+define( 'DB_HOST', '${env.DB_HOST}' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
